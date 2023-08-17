615ce000334845c9039165edd079bc99ffaf1288
MagicianJoshua <joshuajordyn@hotmail.com>
2023-08-17 13:56:10 -0300

    Made the start timer, works perfect, made the script spawn buttons when the start button is pressed, need to find a way to remove them after one of the buttons is clicked or the game is over

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,49 @@
 var timer = document.querySelector(".timer");
+var head = document.querySelector(".mainhead");
 var startButton = document.querySelector(".startButton");
+var time = 10;
+var losses = localStorage.getItem("losses");
+var wins = localStorage.getItem("wins");
+var amountOfButtons = 5;
+var button = document.createElement("button");
 
-startButton.addEventListener("click", function(event){
-    timer.textContent = 100;
-})
 
+// we need a way to have right answers and wrong answers be displayed at the same time
+var questions = ["is javascript dynamically typed language or staticly typed language"];
+var rightAnswers = ["dynamically typed"];
+var wrongAnswers = ["statically typed"];
+
+startButton.addEventListener("click", function (event) {
+  startButton.style.display = "none" ;
+  timer.textContent = time;
+  startTime();
+  startGame();
+});
+
+
+
+function startTime() {
+  var timerInterval = setInterval(function () {
+    time--;
+    timer.textContent = time;
+
+    if (time === 0) {
+      clearInterval(timerInterval);
+      startButton.style.display = "inline";
+    }
+  }, 1000);
+}
+
+function startGame(){
+    for (i = 0; i < amountOfButtons; i++){
+        let button = document.createElement("button");
+        button.setAttribute("id", "quiz-button");
+        head.appendChild(button);
+        button.textContent = "testing";
+        button.style.display = "flex";
+    }
+    
+}
+function initializeQuestions() {
+
+}
